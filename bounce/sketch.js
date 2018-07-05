877165fce9e2e9c0b4b10dec5d6076d1ff21531b
Geredis <NQ.Hung.98gmail.com>
2018-07-05 09:58:05 +0200

    Created a canvas and basic functionality

--- bounce/sketch.js
+++ bounce/sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(1200, 600);
+}
+
+class Ball {
+	constructor(x, y)	{
+		this.x = x;
+		this.y = y;
+		this.initial = y;
+	}
+
+	move()	{
+		this.y -= 10;
+	}
+
+	bounce()	{
+		this.initial /= 2;
+		while(this.y < this.initial)	{
+			this.y += 10;
+		}
+	}
+
+	show(x, y)	{
+		ellipse(this.x, this.y, 15);
+	}
 }
 
 function draw() {
-
+	let ball1 = new Ball(mouseX, mouseY);
+	background(0);
+	ball1.move();
+	ball1.show();
+	if(ball1.x <= 0)	{
+		ball1.bounce();
+	}
 }
