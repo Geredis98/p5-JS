826546d17e90bbe122c1e07f7cfe0f08484181b8
Geredis <NQ.Hung.98gmail.com>
2018-07-06 21:21:05 +0200

    Created a basic gameboard layout

--- xo/sketch.js
+++ xo/sketch.js
@@ -1,13 +1,28 @@
+let board_size = 30;
+let gameboard = new Array(board_size); // Array containing X and O, used for checking winner
+
 function setup()	{
 	createCanvas(750, 750);
+	createGameboard(gameboard, board_size);
+	background(0);
+}
+
+function createGameboard(gameboard, board_size)	{
+	for(let i = 0; i < board_size; i++)	{
+		gameboard[i] = new Array(board_size);
+	}
+}
+
+function drawGameboard()	{
+	stroke(255);
+	let i = 1;
+	while(25 * i < 750)	{
+		line(25 * i, 0, 25 * i, 750);
+		line(0, 25 * i, 750, 25 * i);
+		i++;
+	}
 }
 
 function draw()	{
-	background(0);
-	let i = 0;
-	while(i <= 750)	{
-		fill(255);
-		line(i, 0, i, 750);
-		i += 5;
-	}
+	drawGameboard();
 }
