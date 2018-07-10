4360b2da2f12d2abaaf2f4c2e868182f605a5710
Geredis98 <NQ.Hung.98gmail.com>
2018-07-10 21:09:29 +0200

    Created a two-player version

--- xo/sketch.js
+++ xo/sketch.js
@@ -1,5 +1,6 @@
 let board_size = 30;
 let gameboard = new Array(board_size); // Array containing X and O, used for checking winner
+var person = 1;
 
 function setup()	{
 	background(255);
@@ -19,6 +20,7 @@ function createGameboard(gameboard, board_size)	{
 	}
 }
 
+// Draw lines representing a game grid
 function drawGameboard()	{
 	stroke(0);
 	let i = 1;
@@ -33,16 +35,28 @@ function mousePressed()	{
 	let x = Math.floor(mouseX / 25);
 	let y = Math.floor(mouseY / 25);
 
-	gameboard[x][y] = 1;
+	if(gameboard[x][y] != 0) // Prevents clicking the same tile for a second time
+		return;
+
+	if(person == 1)	{
+		gameboard[x][y] = 1;
+		person = 2;
+	} else {
+		gameboard[x][y] = 2;
+		person = 1;
+	}
 }
 
 function drawTurns(gameboard, board_size)	{
 	for(let i = 0; i < board_size; i++)	{
 		for(let j = 0; j < board_size; j++)	{
+			stroke(5);
 			if(gameboard[i][j] == 1)	{
-				stroke(1);
-				fill(255, 0, 0);
-				ellipse(25 * i + 12, 25 * j + 12, 4, 4);
+				line(25 * i + 7, 25 * j + 7, 25 * i + 17, 25 * j + 17);
+				line(25 * i + 7, 25 * j + 17, 25 * i + 17, 25 * j + 7);
+			} else if(gameboard[i][j] == 2)	{
+				noFill();
+				ellipse(25 * i + 12, 25 * j + 12, 10, 10);
 			}
 		}
 	}
