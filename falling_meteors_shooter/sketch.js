4836cb88f974611a645fa11740f014c99636abfa
Geredis98 <NQ.Hung.98@gmail.com>
2018-07-11 21:11:35 +0200

    Created a Meteor object and a check whether object is shooted

--- falling_meteors_shooter/sketch.js
+++ falling_meteors_shooter/sketch.js
@@ -1,8 +1,8 @@
 var meteor1;
+var shooted = false;
 
 function setup() {
 	createCanvas(750, 750);
-	background(0);
 	meteor1 = new Meteor();
 }
 
@@ -11,7 +11,7 @@ class Meteor	{
 		this.r = random(10, 40);
 		this.x = random(50, 700);
 		this.y = 0;
-		this.speed = random(5, 20);
+		this.speed = random(0.5, 1);
 		this.speedMultiplier = 1;
 	}
 
@@ -19,14 +19,30 @@ class Meteor	{
 		this.y += this.speed * this.speedMultiplier;
 	}
 
+	shoot()	{
+		let distance = dist(mouseX, mouseY, this.x, this.y);
+		if(distance <= this.r)	{
+			shooted = true;
+		}
+	}
+
 	show()	{
 		noStroke();
-		fill(139, 69, 19);	// RGB of saddlebrown
+		if(shooted == false)	{
+			fill(139, 69, 19);
+		} else {
+			fill(0);
+		}
 		ellipse(this.x, this.y, this.r);
 	}
 }
 
+function mousePressed()	{
+	meteor1.shoot();
+}
+
 function draw() {
+	background(0);
 	meteor1.move();
 	meteor1.show();
 }
