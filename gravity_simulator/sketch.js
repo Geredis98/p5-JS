82b8e39f6632e38fb2ed75dc39a0cce24f8a3357
Geredis98 <NQ.Hung.98@gmail.com>
2018-07-13 21:17:38 +0200

    Added Planet object, mousePressed() function

--- gravity_simulator/sketch.js
+++ gravity_simulator/sketch.js
@@ -1,8 +1,32 @@
+let planet;
+let planetCreated = false;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	document.write("Hello world!");
+	createCanvas(750, 750);
 }
 
 function draw() {
+	background(0);
+	if (planetCreated)	{
+		planet.show();
+	}
+}
 
+function mousePressed()	{
+	planet = new Planet(mouseX, mouseY);
+	planetCreated = true;
+}
+
+class Planet	{
+	constructor(x, y)	{
+		this.x = x;
+		this.y = y;
+		this.r = random(10, 50);
+		this.color = color(random(1, 255), random(1, 255), random(1, 255));
+	}
+
+	show()	{
+		fill(this.color);
+		ellipse(this.x, this.y, this.r);
+	}
 }
